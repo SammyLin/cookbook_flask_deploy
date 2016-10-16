945b7eeffef2f1f318aa84965a812b9d05d53559
Sammy Lin <sammylintw@gmail.com>
2016-10-16 13:48:37 +0800

    add gunicorn config setting

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,10 @@
 default['deploy']['path'] = '/opt/my_first_python'
 default['deploy']['source_git_url'] = 'https://github.com/SammyLin/my_first_python.git'
+
+default['deploy']['gunicorn'] = {}
+default['deploy']['gunicorn']['syslog_enable'] = true
+default['deploy']['gunicorn']['syslog_prefix'] = 'gunicorn'
+default['deploy']['gunicorn']['syslog_facility'] = 'daemon'
+default['deploy']['gunicorn']['loglevel'] = 'info'
+default['deploy']['gunicorn']['errorlog'] = '/var/log/gunicorn_error.log'
+default['deploy']['gunicorn']['accesslog'] = '/var/log/gunicorn_access.log'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,16 @@ include_recipe 'poise-python'
 application node['deploy']['path'] do
   git node['deploy']['source_git_url']
   pip_requirements
+
+  template '/etc/gunicorn/gunicorn_config.py' do
+    source 'gunicorn_config.py.erb'
+    mode '0644'
+    variables(gunicorn:  node['deploy']['gunicorn'])
+  end
+
   gunicorn do
     port 9001
+    config '/etc/gunicorn/gunicorn_config.py'
   end
 end
 
